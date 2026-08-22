c3f5a685d5b9e103ace8c744b4ba2f057e0454df
AbdulArfaz <abdularfaz00@gmail.com>
2026-08-22 06:00:53 +0530

    clone the repo

--- packages/client/vite.config.js
+++ packages/client/vite.config.js
@@ -1,11 +1,9 @@
-import path from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-   plugins: [react(), tailwindcss()],
    plugins: [react(), tailwindcss()],
    resolve: {
       alias: {
@@ -14,7 +12,10 @@ export default defineConfig({
    },
    server: {
       proxy: {
-         '/api': 'http://localhost:3000',
+         '/api': {
+            target: 'http://localhost:3000',
+            changeOrigin: true,
+         },
       },
    },
 });
--- packages/server/index.js
+++ packages/server/index.js
@@ -5,6 +5,6 @@ dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(process.env.PORT, () => {
+app.listen(PORT, () => {
    console.log(`Server is running on http://localhost:${PORT}`);
 });
